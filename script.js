1833ffcb5cfe703ed87dca1cbe64b0a680b45597
ArsenyOstapenk <ostapenko.arseniy@mail.ru>
2019-09-04 18:35:30 +0500

    аккордеоны и ховер-меню

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//слайдер
+
 const left = document.querySelector('#left');
 const right = document.querySelector('#right');
 const slider = document.querySelector('#slider');
@@ -35,6 +37,23 @@ left.addEventListener('click', function (e) {
 
 })
 
+//первая секция
+
+const hmenu = document.querySelector('#hmenu');
+const hoverMenu = document.querySelector('#hover-menu');
+const cross = document.querySelector('#cross');
+
+hmenu.addEventListener('click', function (e) {
+   e.preventDefault();
+
+   hoverMenu.style.display = 'block';
+})
+
+cross.addEventListener('click', function (e) {
+   e.preventDefault();
+
+   hoverMenu.style.display = 'none';
+})
 
 // right.addEventListener ('click', function (e) {
 //    e.preventDefault();
@@ -56,3 +75,62 @@ left.addEventListener('click', function (e) {
 //       slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
 //    }
 // };
+
+//accordeon
+
+const menu = document.querySelector('.menu');
+const menuBlock = document.querySelectorAll('.menu__block');
+const menuBlockLenght = menuBlock.length;
+
+menu.addEventListener('click', function (e) {
+   for (let i=0; i<menuBlockLenght; i++){
+      menuBlock[i].classList.remove('menu__block--active');
+   }
+})
+
+for (let i=0; i<menuBlockLenght; i++) {
+   menuBlock[i].addEventListener('click', function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      if (menuBlock[i].classList.contains('menu__block--active')){
+         menuBlock[i].classList.remove('menu__block--active')
+      }else{
+         for(let i=0; i<menuBlockLenght; i++){
+            menuBlock[i].classList.remove('menu__block--active')
+         }
+         menuBlock[i].classList.add('menu__block--active')
+      }
+   })
+}
+
+
+//team
+
+const teamBlockRight = document.querySelector('.team__block-right');
+const teamMember = document.querySelectorAll('.team__member');
+const teamMemberLenght = teamMember.length;
+
+
+teamBlockRight.addEventListener('click', function (e) {
+   for (let i=0; i<teamMemberLenght; i++){
+      teamMember[i].classList.remove('team__member--active');
+   }
+})
+
+for (let i=0; i<teamMemberLenght; i++) {
+   teamMember[i].addEventListener('click', function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      if (teamMember[i].classList.contains('team__member--active')){
+         teamMember[i].classList.remove('team__member--active')
+      }else{
+         for(let i=0; i<teamMemberLenght; i++){
+            // teamMember[i].classList.remove('team__member--active')
+         }
+         teamMember[i].classList.add('team__member--active')
+      }
+   })
+   
+}
